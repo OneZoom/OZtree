363a84b3bd8a65b9b4aa7c840958b9b36e3ad51a
Yan Wong <yan@yanwong.me>
2018-03-15 12:31:55 +0000

    correct assert method

--- tests/functional/treeviewer/test_viewer_errors.py
+++ tests/functional/treeviewer/test_viewer_errors.py
@@ -5,10 +5,11 @@ From https://github.com/OneZoom/OZtree/issues/62
 1) Test no js errors on main page (also that it zooms to the right place) - see http://blog.amolchavan.space/capture-javascript-console-error-using-selenium-webdriver/
 2) Test the iframe popups
 """
+import os.path
+from nose import with_setup
 
 from ...util import base_url
 from ..functional_tests import FunctionalTest
-import os.path
 
 class TestViewerErrors(FunctionalTest):
     """
@@ -16,15 +17,15 @@ class TestViewerErrors(FunctionalTest):
     """
     @classmethod
     def setUpClass(self):
-        print("Running {}".format(os.path.basename(__file__)))
+        print("== Running {} ==".format(os.path.basename(__file__)))
         super().setUpClass()
-    
+
     def test_viewer_embedded(self):
         """
         Do we get the proper error page if we accidentally embed a viewer within another viewer
         """
         self.browser.get(base_url + 'life?embed=3')
-        self.assertTrue(self.browser.find_element_by_id('stop_recursion'))
+        assert self.element_by_id_exists('stop_recursion')
 
             
     def test_viewer_normal(self):
@@ -35,7 +36,7 @@ class TestViewerErrors(FunctionalTest):
             self.browser.get(base_url + page)
             ### to do
         
-    def test_image_popup():
+    def test_image_popup(self):
         """
         Here we should test that we can pop up local (OZ) information about an image (esp. in museum display mode)
         """
