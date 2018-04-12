47609a9a3e559fda51bea70867b27d16d39ec6a1
Yan Wong <yan@yanwong.me>
2018-04-12 12:26:42 +0100

    Add some more DB tests

--- tests/consistency/test_database_settings.py
+++ tests/consistency/test_database_settings.py
@@ -17,10 +17,38 @@ class TestDatabaseSettings(object):
     def tearDownClass(self):
         pass
     
+    def test_fulltext_index_settings(self):
+        """Check that we have the right minimum word size for fulltext indexes"""
+        db_cursor = self.db['connection'].cursor()
+        db_cursor.execute("SELECT @@GLOBAL.innodb_ft_min_token_size")
+        innodb_ft_min_token_size = db_cursor.fetchone()[0]
+        self.db['connection'].commit() #need to commit here otherwise next select returns stale data
+        assert innodb_ft_min_token_size <= 3, "innodb_ft_min_token_size should be 3 characters max for decent search results"
+        db_cursor.close()
+        
+        
+    def test_full_unicode_for_vernaculars(self):
+        """Vernacular names (which include chinese etc) should be full 4 byte unicode"""
+        check_table_colname = { #the list of table:colname to check for identity
+            'vernacular_by_ott': 'vernacular', 
+            'vernacular_by_name':'vernacular',
+            }
+        name_info = {}
+        
+        for table, colname in check_table_colname.items():
+            db_cursor = self.db['connection'].cursor()
+            db_cursor.execute("SELECT character_set_name FROM information_schema.`COLUMNS` WHERE table_schema = SCHEMA() AND table_name = '{}' AND column_name = '{}';".format(table, colname))
+            name_info[table+"-"+colname] = db_cursor.fetchone()
+            self.db['connection'].commit() #need to commit here otherwise next select returns stale data
+        db_cursor.close()
+        for k,v in name_info.items():
+            assert v[0] == "utf8mb4", "vernacular columns should be full 4 byte unicode"       
+    
     def test_name_indexes_use_same_charset(self):
         """
-        The 'name' columns in e.g. vernacular_by_name & ordered_leaves are used as unique keys. If they are different charsets it can drastically slow down search.
-        This has previously caused problems
+        Check the 'name' columns in e.g. vernacular_by_name & ordered_leaves have identical charsets
+        Since these are used as unique matching keys, having different charsets can drastically slow down search,
+        which has caused problems in the past
         """
         
         check_table_colname = { #the list of table:colname to check for identity
@@ -36,7 +64,7 @@ class TestDatabaseSettings(object):
             db_cursor.execute("SELECT character_set_name, column_type FROM information_schema.`COLUMNS` WHERE table_schema = SCHEMA() AND table_name = '{}' AND column_name = '{}';".format(table, colname))
             name_info[table+"-"+colname] = db_cursor.fetchone()
             self.db['connection'].commit() #need to commit here otherwise next select returns stale data
-        
+        db_cursor.close()
 
         for k,v in name_info.items():
             m = re.match(r'varchar\((\d+)\)', v[1])
