c4fcc04e28f3f5d05f587acafd5db63417834e4a
Yan Wong <yan@yanwong.me>
2018-04-13 12:39:26 +0100

    add blank test for language (to be filled out)

---
+++ tests/functional/treeviewer/test_language_localisation.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Test that loading or switching between languages works
+"""
+import sys
+import os.path
+from time import sleep
+
+
+from ...util import base_url
+from ..functional_tests import FunctionalTest, linkouts_url, has_linkouts
+
+class TestLanguageLocalisation(FunctionalTest):
+    """
+    TO DO
+    """
+    @classmethod
+    def setUpClass(self):
+        print("== Running {} ==".format(os.path.basename(__file__)))
+        super().setUpClass()
+    
+    
