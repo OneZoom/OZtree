08117c6235504cbf7d7fef5ca9af83d34585b81c
Jamie Lentin <jm@lentin.co.uk>
2018-06-14 10:56:22 +0100

    otop: Background floating trees should be smaller, less opaque and more spaced out #90

--- static/otop/backgrounds.js
+++ static/otop/backgrounds.js
@@ -53,6 +53,7 @@ function particles(background_el, url) {
         el = tinted_image(url, [255, 255, 255], rand_int(-15, -5), rand_int(-15, -5), 120);
         el.style.opacity = 0;
         el.style.transform = 'perspective(2000px) rotateX(10deg) rotateZ(' + rand_int(0, 360) + 'deg)';
+        el.style.transition = 'transform 30s ease-out, opacity 15s';
         el.classList.add('particles');
         background_el.appendChild(el);
     }
@@ -60,36 +61,48 @@ function particles(background_el, url) {
     window.setTimeout(function () {
         var start_rotate = parseInt(el.style.transform.match(/rotateZ\((\d+)/)[1], 10);
         start_rotate += 3*rand_int(10, 15);
-        el.style.opacity = 0.75;
+        el.style.opacity = 0.6;
         el.style.transform = 'perspective(2000px) rotateX(10deg) scale('+ rand_int(10,15)/10 +') rotateZ(' + start_rotate + 'deg)';
     }, 50);
 
     window.setTimeout(particles.bind(this, background_el, url), 25000);
 }
 
-function floating(background_el, url, initial) {
+function floating(background_el, url, initial_spacing) {
     var css_class = 'floating_' + url.replace(/\W/g, '_'),
-        el; // = background_el.querySelector('.' + css_class);
+        start_horiz = rand_int(-20, 120),
+        el;
 
     if (!el) {
-        el = tinted_image(url, tints[rand_int(0, tints.length - 1)], 0, rand_int(0, 10), 80);
+        el = tinted_image(url, tints[rand_int(0, tints.length - 1)], 0, rand_int(0, 10), 50);
         el.style.opacity = 0;
-        el.style.transform = 'translate(' + rand_int(-10, 10) + '%, ' + rand_int(initial ? 50 : 70, 80) + '%)';
+        el.style.transform = [
+            'translate(' + start_horiz + '%, ' + rand_int(initial_spacing * 120, initial_spacing * 130) + '%)',
+            'scale(' + ((1 - initial_spacing) * 2 + 1) + ')',
+        ].join(' ');
+        el.style.transition = [
+            'transform ' + initial_spacing * 240 + 's linear',
+            'opacity 15s',
+        ].join(',');
         el.classList.add('floating');
         el.classList.add(css_class);
         background_el.appendChild(el);
     }
 
     window.setTimeout(function () {
-        el.style.opacity = 0.6;
-        el.style.transform = 'rotate(' + rand_int(-45, 45) + 'deg) translate(' + rand_int(-20,100) + '%, -100%)';
+        el.style.opacity = 0.5;
+        el.style.transform = [
+            'rotate(' + rand_int(-90, 90) + 'deg)',
+            'translate(' + (start_horiz + rand_int(-20,20)) + '%, -100%)',
+            'scale(1)',
+        ].join(' ');
     }, 50);
 
-    window.setTimeout(floating.bind(this, background_el, url, false), 45000);
+    window.setTimeout(floating.bind(this, background_el, url, 1), initial_spacing * 45000);
 
     window.setTimeout(function () {
         el.style.opacity = 0;
-    }, 180000);
+    }, initial_spacing * 180000);
 
     window.setTimeout(function () {
         background_el.removeChild(el);
@@ -107,7 +120,7 @@ function init_background(images) {
     document.body.insertBefore(background_el, document.body.firstChild);
 
     for (i = 0 ; i < images['tree'].length; i++) {
-        floating(background_el, images['tree'][i], true);
+        floating(background_el, images['tree'][i], i / images['tree'].length);
     }
     particles(background_el, choose_image('particle'));
 }
