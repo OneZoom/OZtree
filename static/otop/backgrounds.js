cc063a3af6f26a99176d59b2aad91d7a9bd2f405
Jamie Lentin <jm@lentin.co.uk>
2018-06-14 17:40:21 +0100

    otop: The edges of particles were becoming visible #90

--- static/otop/backgrounds.js
+++ static/otop/backgrounds.js
@@ -26,10 +26,18 @@ function particles(background_el, url) {
     var el = background_el.querySelector('.particles');
 
     if (!el) {
-        el = image_div(url, rand_int(-15, -5), rand_int(-15, -5), 120);
+        el = image_div(url, -20, -20, 120);
+        el.style.backgroundSize = 'cover';
         el.style.opacity = 0;
-        el.style.transform = 'perspective(2000px) rotateX(10deg) rotateZ(' + rand_int(0, 360) + 'deg)';
-        el.style.transition = 'transform 30s ease-out, opacity 15s';
+        el.style.transform = [
+            'perspective(2000px)',
+            'rotateX(10deg)',
+            'rotateZ(' + rand_int(0, 360) + 'deg)',
+        ].join(' ');
+        el.style.transition = [
+            'transform 30s ease-out',
+            'opacity 15s',
+        ].join(',');
         el.classList.add('particles');
         background_el.appendChild(el);
     }
@@ -38,7 +46,12 @@ function particles(background_el, url) {
         var start_rotate = parseInt(el.style.transform.match(/rotateZ\((\d+)/)[1], 10);
         start_rotate += 3*rand_int(10, 15);
         el.style.opacity = 0.6;
-        el.style.transform = 'perspective(2000px) rotateX(10deg) scale('+ rand_int(10,15)/10 +') rotateZ(' + start_rotate + 'deg)';
+        el.style.transform = [
+            'perspective(2000px)',
+            'rotateX(10deg)',
+            'scale('+ rand_int(10,15)/10 +')',
+            'rotateZ(' + start_rotate + 'deg)',
+        ].join(' ');
     }, 50);
 
     window.setTimeout(particles.bind(this, background_el, url), 25000);
