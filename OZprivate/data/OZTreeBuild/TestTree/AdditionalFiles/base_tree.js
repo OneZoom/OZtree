d73ce9f50237582e0f4cf4986360fd4bec7848e2
Yan Wong <yan@yanwong.me>
2018-08-05 19:06:19 +0100

    add new test plant dataset

---
+++ OZprivate/data/OZTreeBuild/TestTree/AdditionalFiles/base_tree.js
@@ -0,0 +1,10 @@
+//;# Various hacks allow this javascript code to be included in perl simply using > require ('filename.js');
+//;# this  includes using '//;#' as a single-line comment, and prefixing variables by '$'
+
+//;# Base tree for chordates, assuming initial divergence at 550Mya.
+
+$tree = new Tree('BespokeTree/include_files/GreatApes.PHY');
+
+//;# PHYLOGENIES PART FILLED BY OPEN TREE OF LIFE
+//;# Commands output from getOpenTreesFromOneZoom.py
+
