13597d8f82b54f22473f5bb379c7ab56b8ab60f5
Yan Wong <yan@yanwong.me>
2018-10-27 21:13:03 +0100

    Wrong OrderedDict call signature

--- OZprivate/ServerScripts/TaxonMappingAndPopularity/OTT_popularity_mapping.py
+++ OZprivate/ServerScripts/TaxonMappingAndPopularity/OTT_popularity_mapping.py
@@ -304,8 +304,8 @@ def add_wikidata_info(source_ptrs, wikidata_json_dump_file, wikilang, verbosity=
     override_with_common_name = [5] #for humans (Q5) use the sitelinks from the common name item, even though the taxon item exists
     #numbers to search for
     match_Qtypes = {}
-    match_taxa = OrderedDict(('taxon', 16521), ('monotypic taxon', 310890), ('fossil taxon', 23038290), ('clade',713623))
-    match_common_names = OrderedDict(('common name', 502895), ('group of organisms known by one particular common name', 55983715))
+    match_taxa = OrderedDict((('taxon', 16521), ('monotypic taxon', 310890), ('fossil taxon', 23038290), ('clade',713623)))
+    match_common_names = OrderedDict((('common name', 502895), ('group of organisms known by one particular common name', 55983715)))
     match_Qtypes.update(match_taxa)
     match_Qtypes.update(match_common_names)
     initial_byte_match = re.compile('numeric-id":(?:{})\D'.format('|'.join([str(v) for v in match_Qtypes.values()])).encode()) 
