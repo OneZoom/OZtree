a22f8513e7e82360779d1adc0a67bcdca28f233d
kz12 <kz12@ic.ac.uk>
2019-08-13 17:35:23 +0800

    Add hash number in OZEntry filename.
    This result in generating a different filename for OZEntry.js whenever its content changes. This could solve the issue of user's js file not updated due to file cache.
    
    OZEntry may not end up compressed and uglified because grunt file does not know the exact generated name. Now uglify and compress process is done by webpack.

--- Gruntfile.js
+++ Gruntfile.js
@@ -197,5 +197,5 @@ module.exports = function (grunt) {
   ]);
   grunt.registerTask("precompile-docs", ["jsdoc2md", "exec:precompile_docs"]);
   grunt.registerTask("build", ["clean:build", "precompile-python", "precompile-js", "copy:old_js", "compass", "uglify", "compress", "precompile-docs"]);
-  grunt.registerTask("compile", ["clean:compile", "precompile-js_dev", "copy:old_js", "compass" , "compress", "copy:to_live", "precompile-docs"]);
+  grunt.registerTask("compile", ["clean:compile", "precompile-js", "copy:old_js", "compass", "compress", "copy:to_live", "precompile-docs"]);
 };
