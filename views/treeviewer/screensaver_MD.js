730076dd81bcbe76e20ff28030e399012b382ff7
Yan Wong <yan@yanwong.me>
2019-09-19 00:41:27 +0100

    Proper screensave format

--- views/treeviewer/screensaver_MD.js
+++ views/treeviewer/screensaver_MD.js
@@ -1,38 +1,39 @@
 /* DEFINE SOME SETTINGS TO BE PASSED TO THE SCREENSAVER */
-"general": {
-    "dom_names": {
-        /**
-         * All the following values are default values.
-         */
-        "wrapper_id": "tour_wrapper",
-        "next_class": "tour_next",
-        "prev_class": "tour_prev",
-        "exit_class": "tour_exit",
-        "exit_confirm_class": "exit_confirm",
-        "exit_cancel_class": "exit_cancel"
+{
+    "general": {
+        "dom_names": {
+            /**
+             * All the following values are default values.
+             */
+            "wrapper_id": "tour_wrapper",
+            "next_class": "tour_next",
+            "prev_class": "tour_prev",
+            "exit_class": "tour_exit",
+            "exit_confirm_class": "exit_confirm",
+            "exit_cancel_class": "exit_cancel"
+        },
     },
-},
-"tourstop_shared": {
-    "template": "static/tour/tour_template.html",
-    "template_style": "static/tour/tour_template.css",
-    "update_class": {
-        "title": "OneZoom Demo Tour",
-        // Don't show any boxes
-        "container": {"style": {"display": "None"}}
-    }
-},
-"tourstops": [
-    {
-        "ott": "991547",
-        "wait": 1000
+    "tourstop_shared": {
+        "template": "static/tour/tour_template.html",
+        "template_style": "static/tour/tour_template.css",
+        "update_class": {
+            "title": "OneZoom Demo Tour",
+            // Don't show any boxes
+            "container": {"style": {"display": "None"}}
+        }
     },
-    {
-        "ott": "81461",
-        "wait": 1000
-    },
-    {
-        "ott": "99252",
-        "wait": 1000
-    },
-]
+    "tourstops": [
+        {
+            "ott": "991547",
+            "wait": 1000
+        },
+        {
+            "ott": "81461",
+            "wait": 1000
+        },
+        {
+            "ott": "99252",
+            "wait": 1000
+        },
+    ]
 }
