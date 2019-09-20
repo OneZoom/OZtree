d72e0c9407fce6ebe17015f1f09fb7bdd9246c72
Yan Wong <yan@yanwong.me>
2019-09-20 22:20:38 +0100

    Add compress to compile routine

--- Gruntfile.js
+++ Gruntfile.js
@@ -188,6 +188,6 @@ module.exports = function (grunt) {
     "exec:partial_local_install:otop.html:minotop.html",
   ]);
   grunt.registerTask("precompile-docs", ["jsdoc2md", "exec:precompile_docs"]);
-  grunt.registerTask("build", ["clean:build", "precompile-python", "precompile-js", "copy:old_js", "compass","uglify", "compress","precompile-docs"]);
-  grunt.registerTask("compile", ["clean:compile", "precompile-js_dev", "copy:old_js", "compass" , "copy:to_live", "precompile-docs"]);
+  grunt.registerTask("build", ["clean:build", "precompile-python", "precompile-js", "copy:old_js", "compass", "uglify", "compress", "precompile-docs"]);
+  grunt.registerTask("compile", ["clean:compile", "precompile-js_dev", "copy:old_js", "compass" , "compress", "copy:to_live", "precompile-docs"]);
 };
