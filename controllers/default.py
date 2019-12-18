0c91ce474a40104ba747108afcf0ddcecc9f020b
Yan Wong <yan@yanwong.me>
2019-12-18 20:00:10 +0000

    Correct sponsor count

--- controllers/default.py
+++ controllers/default.py
@@ -105,7 +105,7 @@ def index():
                 for ott,vn
                 in get_common_names([r.OTT_ID for r in sponsored_rows], return_nulls=True).items()}
         ),
-        n_total_sponsored=db((db.reservations.verified_time != None) & ((db.reservations.deactivated == None) | (db.reservations.deactivated == ""))).count(distinct=db.reservations.user_registration_id),
+        n_total_sponsored=db(db.reservations.PP_e_mail != None).count(distinct=db.reservations.PP_e_mail),
         n_sponsored_leaves=db((db.reservations.verified_time != None) & ((db.reservations.deactivated == None) | (db.reservations.deactivated == ""))).count(),
         popular_places=[
             dict(
