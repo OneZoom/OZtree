e5aab88b62d15a6e639a7afdbd42e036ccdde6e0
Yan Wong <yan@yanwong.me>
2020-01-10 13:17:22 +0000

    Nicer representation of table.column

--- tests/site_setup/test_database_settings.py
+++ tests/site_setup/test_database_settings.py
@@ -90,16 +90,15 @@ class TestDatabaseSettings(unittest.TestCase):
         with self.db['connection'].cursor() as db_cursor:
             for table, colname in check_table_colname.items():
                 db_cursor.execute("SELECT character_set_name, column_type FROM information_schema.`COLUMNS` WHERE table_schema = SCHEMA() AND table_name = '{}' AND column_name = '{}';".format(table, colname))
-                name_info[table+"-"+colname] = db_cursor.fetchone()
+                name_info[table+"."+colname] = db_cursor.fetchone()
                 self.db['connection'].commit() #need to commit here otherwise next select returns stale data
 
         for k,v in name_info.items():
             m = re.match(r'varchar\((\d+)\)', v[1])
-            assert m, "Table-column {} must be of type varchar, but is {}".format(k, v[1])
-            assert int(m.group(1)) < 200, "Table-column {} is of length {} but should be less than {} ({}), to allow mb4 indexing (see {})".format(
+            assert m, "Table.column {} must be of type varchar, but is {}".format(k, v[1])
+            assert int(m.group(1)) < 200, "Table.column {} is of length {} but should be less than {} ({}), to allow mb4 indexing (see {})".format(
                 k, 767/4, "767/4", v[1], "https://dev.mysql.com/doc/refman/5.7/en/innodb-restrictions.html")
-                
-                
+
         test_charset = None
         ni = None
         for k,v in name_info.items():
@@ -107,7 +106,7 @@ class TestDatabaseSettings(unittest.TestCase):
                 test_charset = v[0]
                 ni = k
             else:
-                assert v[0] == test_charset, "Table-column {} (charset '{}') is different to {} (charset '{}')".format(
+                assert v[0] == test_charset, "Table.column {} (charset '{}') is different to {} (charset '{}')".format(
                     k, v[0], ni, test_charset)
 
     
