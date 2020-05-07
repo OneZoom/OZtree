6c91e45c32dbe1c82228f0b80c400f5c9915f79b
Jamie Lentin <jm@lentin.co.uk>
2020-05-07 17:04:31 +0100

    src/global_config Handy debug to time (and profile) tree load #341

--- OZprivate/rawJS/OZTreeModule/src/global_config.js
+++ OZprivate/rawJS/OZTreeModule/src/global_config.js
@@ -86,7 +86,16 @@ config.ui = {
      OneZoom code */
   closeAll: function () {/* by default, do nothing */ },
   loadingMessage: function (active) {
-    if (window.is_testing) console.log(active ? "Page loading..." : "Finished loading.");
+    if (window.is_testing) {
+      console.log(active ? "Page loading..." : "Finished loading.");
+      if (active) {
+        console.time("Page load");
+        //console.profile("Page load");
+      } else {
+        console.timeEnd("Page load");
+        //console.profileEnd();
+      }
+    }
   },
   badOTT: function (ott) {
     alert('You have passed in a bad name or number, so we have\n' +
