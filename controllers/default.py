ff9b2bc26d9a27af621a90a78ed977718b2ed20c
Jamie Lentin <jm@lentin.co.uk>
2022-03-21 13:48:23 +0000

    embed_edit: Add new embed editor #283
    Add new embed editor modelled on old embed editor. No keys attached to
    URLs or database storage yet.

--- controllers/default.py
+++ controllers/default.py
@@ -1607,6 +1607,29 @@ def pp_process_post():
     else:
         return(dict(vars=request.vars, args=request.args))
 
+def embed_edit():
+    form = FORM(
+        LABEL("E-mail address"),
+        INPUT(_type='email', requires=[IS_NOT_EMPTY(), IS_EMAIL()], _name='email', _class="uk-input uk-margin-bottom"),
+        INPUT(_type='hidden', _name='url', _value=URL('life', scheme=True, host=True)),
+        INPUT(_type='submit', _value="Send e-mail", _class="oz-pill pill-leaf"),
+        _id="form_embed_email",
+    )
+
+    if form.accepts(request.vars, session=None, keepvalues=True):
+        mail, reason=ozmail.get_mailer()
+        if mail is None:
+            response.flash = '%s, so cannot send email' % reason
+        else:
+            mailargs = ozmail.template_mail('embed_code', dict(
+                url=form.vars.url,
+            ), to=form.vars.email)
+            mail.send(**mailargs)
+            response.flash = "E-mail with embed code sent"
+    return dict(
+        form=form,
+    )
+
 """ these empty controllers are for other OneZoom pages"""
 
 def introduction():
