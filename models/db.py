026a161c39944c50ec537f7162963d9ccda3a7df
Jamie Lentin <jm@lentin.co.uk>
2022-05-20 10:36:09 +0100

    models: Fix qs_opts syntax #536

--- models/db.py
+++ models/db.py
@@ -768,7 +768,7 @@ db.define_table('tour',
 db.define_table('tourstop',
     Field('ott', type='integer', notnull=False),  # The OTT this tourstop points at. NULL => return to start
     Field('secondary_ott', type='integer', notnull=True),  # A second OTT when targeting a common ancestor
-    Field('qs_opts', type='string', notnull=False, default='')  # QS-style options to apply to modify tourstop, e.g. into_node=true&initmark=...
+    Field('qs_opts', type='string', notnull=False, default=''),  # QS-style options to apply to modify tourstop, e.g. into_node=true&initmark=...
     Field('author', type='text', notnull=True, default=''),  # Author of tourstop (doesn't necessarily match tour in case of remix)
     Field('transition_in', type='string', notnull=True, requires=IS_IN_SET(('fly', 'leap', 'fly_straight'))),  # Transition to use when flying to stop
     Field('fly_in_speed', type='double', notnull=False, default=1),  # Speed relative to global_anim_speed
