be1179e126fcb3bb1348d6a68b2fceedbd9fb4c8
Jamie Lentin <jm@lentin.co.uk>
2022-11-12 10:22:31 +0000

    tour/HandlerYoutube: Support data-ts_autoplay #535
    Support autoplay as-per per commit 73244913.

--- OZprivate/rawJS/OZTreeModule/src/tour/HandlerYoutube.js
+++ OZprivate/rawJS/OZTreeModule/src/tour/HandlerYoutube.js
@@ -12,6 +12,8 @@ function handler(tour) {
   el_yts.forEach((el_yt) => {
     el_yt.el_tourstop = el_yt.closest('.tour > .container');
     el_yt.el_tourstop.classList.add('contains-youtube');
+    // Split attribute into array, filter off empty entries (i.e. when attribute itself is empty)
+    el_yt.autoplay_states = (el_yt.getAttribute('data-ts_autoplay') || '').split(" ").filter(x => x);
   });
 
   return new Promise((resolve) => {
@@ -40,7 +42,7 @@ function handler(tour) {
 
           if (event.data == YT.PlayerState.ENDED) {
             tourstop.block_remove(block_name);
-          } else if (event.data == YT.PlayerState.PLAYING) {
+          } else if (event.data == YT.PlayerState.PLAYING && tourstop.state === iframe.autoplay_states.slice(-1)[0]) {
             tourstop.block_add(block_name);
           }
           // NB; Ignore other events, particularly UNSTARTED since this happens after end
@@ -49,22 +51,23 @@ function handler(tour) {
     }))));
   }).then(() => {
     // Attach observers for any autoplaying video
-    tour.tourstop_observer(
-      ".contains-youtube",
-      ['tsstate-active_wait'],
-      function (tour, tourstop, el_ts) {
-        // Start video playing,
-        el_ts.querySelectorAll(":scope iframe.embed-youtube").forEach((el_yt) => {
-          window.YT.get(el_yt.id).playVideo();
-        });
-      },
-      function (tour, tourstop, el_ts) {
-        // Leaving tourstop, stop any video
-        el_ts.querySelectorAll(":scope iframe.embed-youtube").forEach((el_yt) => {
-          window.YT.get(el_yt.id).stopVideo();
-        });
-      },
-    );
+    tour.tourstop_observer('.contains-youtube', '*', (tour, tourstop, el_ts) => {
+      el_ts.querySelectorAll(":scope iframe.embed-youtube").forEach((el) => {
+        if (window.getComputedStyle(el_ts).visibility !== 'visible') {
+          // Shouldn't ever play whilst invisible
+          window.YT.get(el.id).stopVideo();
+        } else if (el.autoplay_states.indexOf(tourstop.state) > -1) {
+          window.YT.get(el.id).playVideo();
+
+          // Check to see if final block should be added
+          if (tourstop.state === el.autoplay_states.slice(-1)[0]) {
+            if (window.YT.get(el.id).getPlayerState() === YT.PlayerState.PLAYING) {
+              tourstop.block_add(el.src);
+            }
+          }
+        }
+      });
+    });
   });
 }
 
