2092b26dfd45011734e441dacb79643ccb676d35
Jamie Lentin <jm@lentin.co.uk>
2023-04-17 14:55:33 +0100

    controller/controller_dom: Don't lookup largest node if unused

--- OZprivate/rawJS/OZTreeModule/src/controller/controller_dom.js
+++ OZprivate/rawJS/OZTreeModule/src/controller/controller_dom.js
@@ -119,11 +119,10 @@ export default function (Controller) {
 
       // Get pre-rebuild state, so we can restore the rough position by ID
       // Get largest node, use this to restore position
-      let n = get_largest_visible_node(this.root);
-      let prev_ozid = n.ozid;
+      let n = !init ? get_largest_visible_node(this.root) : null;
 
       tree_settings.rebuild_tree(vis, prev, this).then(function () {
-        if (!init) return(self.init_move_to(prev_ozid, "leap"));
+        if (!init) return(self.init_move_to(n.ozid, "leap"));
       }.bind(this));
     }
   }
