OneZoom/OZtree

commit 1d775cbba4a702ad34b15b1db54fd1861ddfd118

navigation/utils: Refactor get_largest_visible_node for readability #538 #547
* Move dependency on tree_state out to controller, where we already
have one
* Unpick if/return
* Combine loops over child nodes

However, the algorithm is pretty flawed I think, and essentially
amounts to the highest node that has gvar set.

OZprivate/rawJS/OZTreeModule/src/navigation/utils.js
@@ -1,62 +1,55 @@
-import tree_state from '../tree_state';
-
 /**
  * Get largest visible node on the screen which meets the condition.
  * If part of the node is not on the screen and part of it is on, only the part that is on the screen counts.
  *
  * @param {Object} node Start point in search, probably controller.root
+ * @param width Width of treeviewer, tree_state.widthres
+ * @param height Height of treeviewer, tree_state.heightres
  * @param {Function} condition Function taking a node as an argument, and returning true,
  *  if the node would qualify as the sort that can be returned. If `null` find the
  *  largest of all the nodes on the screen
  * @param Largest node that matches condition, or null if none found
  */
-function get_largest_visible_node(node, condition=null) {
-  if (!node) return null;  // Trying to record_url() before the tree is setup
+function get_largest_visible_node(node, width, height, condition=null) {
   let condition_satisfy = !condition || condition(node);
 
   function get_area_in_screen(node) {
-    if (!node.gvar) {
-      return 0;
-    } else {
-      let sx = node.xvar + (node.rvar * node.hxmin);
-      let ex = node.xvar + (node.rvar * node.hxmax);
-      let sy = node.yvar + (node.rvar * node.hymin);
-      let ey = node.yvar + (node.rvar * node.hymax);
-      let width_r = Math.max((Math.min(ex, tree_state.widthres) - Math.max(sx, 0)) / (ex - sx), 0);
-      let height_r = Math.max((Math.min(ey, tree_state.heightres) - Math.max(sy, 0)) / (ey - sy), 0);
-      return node.rvar * width_r * height_r;
-    }
+    if (!node.gvar) return 0;
+
+    // Node-and-descendant bounding box
+    let sx = node.xvar + (node.rvar * node.hxmin);
+    let ex = node.xvar + (node.rvar * node.hxmax);
+    let sy = node.yvar + (node.rvar * node.hymin);
+    let ey = node.yvar + (node.rvar * node.hymax);
+
+    // Proportion of bounding box on-screen
+    let width_r = Math.max((Math.min(ex, width) - Math.max(sx, 0)) / (ex - sx), 0);
+    let height_r = Math.max((Math.min(ey, height) - Math.max(sy, 0)) / (ey - sy), 0);
+
+    return node.rvar * width_r * height_r;
   }
 
-  if (node.gvar && condition_satisfy) {
-    return node;
-  } else if (node.dvar) {
-    //otherwise try to find node with ott in its children
-    if (node.has_child) {
-      let satisfied_children = [];
-      satisfied_children = node.children.map(function (child) {
-        return get_largest_visible_node(child, condition);
-      });
-      let largest_area = -1, largest_node = null;
-      let length = satisfied_children.length;
-      for (let i = 0; i < length; i++) {
-        let child = satisfied_children[i];
-        if (child) {
-          let area = get_area_in_screen(child);
-          if (area > largest_area) {
-            largest_area = area;
-            largest_node = child;
-          }
-        }
-      }
+  // Node itself is visible and matches condition, return
+  if (node.gvar && condition_satisfy) return node;
+  // No children are visible, so no nodes here
+  if (!node.dvar) return null;
 
-      if (largest_node !== null) {
-        return largest_node;
-      } else if (!isNaN(node.xvar) && condition_satisfy) {
-        return node;
-      }
+  //otherwise try to find node with ott in its children
+  let largest_node = null, largest_area = -1;
+  node.children.forEach((child) => {
+    const child_largest = get_largest_visible_node(child, condition);
+    if (!child_largest) return;
+
+    const area = get_area_in_screen(child_largest);
+    if (area > largest_area) {
+      largest_node = child_largest;
+      largest_area = area;
     }
-  }
+  });
+  if (largest_node) return largest_node;
+
+  if (!isNaN(node.xvar) && condition_satisfy) return node;
   return null;
 }
+
 export { get_largest_visible_node };

OZprivate/rawJS/OZTreeModule/src/controller/controller_navigation.js
@@ -13,11 +13,17 @@ import tree_state from '../tree_state';
 export default function (Controller) {
   /**
    * Return the current largest visible node, that optionally meets condition
+   *
+   * Debug helper: You can set a watch in the javascript console:
+   *   n = onezoom.controller.largest_visible_node() ; n.cname + " - " + n.latin_name
+   *
    * @param {function} Function returning true iff a node should be considered, e.g. "(node) => node.ott" for only nodes with an OTT
    * @return A midnode object, if available.
    */
   Controller.prototype.largest_visible_node = function (condition=null) {
-    return get_largest_visible_node(this.root, condition);
+    if (!this.root) return null;  // Trying to record_url() before the tree is setup
+
+    return get_largest_visible_node(this.root, tree_state.widthres, tree_state.heightres, condition);
   }
 
   /**

commit b268d05be052600d7d93cbee43128cf93f99886b

navigation/utils: Pass through width/height when recursing #538 #547
1d775cbb broke current location recording, since recursing to child
nodes wouldn't work. Fix the recursive call.

This can admittedly be unit-tested now, but #547 wants to entirely
re-think how this works anyway.

OZprivate/rawJS/OZTreeModule/src/navigation/utils.js
@@ -37,7 +37,7 @@ function get_largest_visible_node(node, width, height, condition=null) {
   //otherwise try to find node with ott in its children
   let largest_node = null, largest_area = -1;
   node.children.forEach((child) => {
-    const child_largest = get_largest_visible_node(child, condition);
+    const child_largest = get_largest_visible_node(child, width, height, condition);
     if (!child_largest) return;
 
     const area = get_area_in_screen(child_largest);