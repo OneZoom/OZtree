59033dd260bcf9d3d0dfe110f09643c8a1a92acf
Jamie Lentin <jm@lentin.co.uk>
2024-04-23 11:24:40 +0000

    modules/markdown: C+P Mishap

--- modules/markdown.py
+++ modules/markdown.py
@@ -8,14 +8,3 @@ def markdown(text):
         )
 
     return current.oz_markdown.convert(text)
-
-if __name__ == '__main__':
-    import sys
-
-    if current.globalenv['is_testing'] != True:
-        raise RuntimeError("Do not run tests in production environments, ensure is_testing = True")
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestEmbed))
-    result = unittest.TextTestRunner(verbosity=2).run(suite)
-    if not result.wasSuccessful():
-        sys.exit(1)
