943c13e087d221a60c5c97526d4091f84a2090e2
Yan Wong <yan.wong@bdi.ox.ac.uk>
2025-06-11 15:17:22 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,18 @@ const path = require('path');
 
 partial_install_site = "http://www.onezoom.org";
 partial_local_install_site = "http://127.0.0.1:8000"; // if you are running a local installation
-preferred_python3 = "python3.10"; // in case you have multiple python3 versions installed
 version_uikit = "3.21.13";
 web2py_py = path.join(path.dirname(path.dirname(process.cwd())), 'web2py.py');
 venv_python = path.join(path.dirname(path.dirname(process.cwd())), 'bin/python');
+
+const { execSync } = require('child_process');
+let preferred_python3;
+try {
+  execSync('python3.10 --version', { stdio: 'ignore' });
+  preferred_python3 = "python3.10";  // in case you have multiple python3 versions installed
+} catch (error) {
+  preferred_python3 = "python3";
+}
 
 /** Generate a function to execute a web2py script, handing over all arguments */
 function exec_web2py_script(script_name, init_args) {
