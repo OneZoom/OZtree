31b170495796d90c9e6507900e1033362eea1165
Yan Wong <yan.wong@bdi.ox.ac.uk>
2025-07-10 15:11:01 +0100

    Add a different colour theme for the extinct tree

--- OZprivate/rawJS/OZTreeModule/src/themes/index.js
+++ OZprivate/rawJS/OZTreeModule/src/themes/index.js
@@ -3,15 +3,9 @@
  * (look at natural_theme.js for an example), then add it to this file. It should be picked up
  * as one of the options available for the OneZoom object (in tree_settings.js)
  **/
-export {default as natural} from './natural_theme';
-export {default as natural_CBF} from './natural_colour_blind_friendly';
-export {default as AT} from './at_theme';
-export {default as AT_CBF} from './at_colour_blind_friendly';
-export {default as otop} from './otop_theme';
+export {default as extinct} from './extinct_theme';
 export {default as popularity} from './popularity_theme';
 export {default as popularity_CBF} from './popularity_colour_blind_friendly';
-export {default as IUCN} from './IUCN_explicit_theme';
-export {default as IUCN_CBF} from './IUCN_explicit_colour_blind_friendly';
 
 export {default as gencons} from './genetic_conservation_theme';
 export {default as gencons_CBF} from './genetic_conservation_theme';
